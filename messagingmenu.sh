f23d10528ca999910e618b9da1fbaf932d3edf1c
Christian Lauinger <christian.lauinger@gmail.com>
2026-01-01 14:55:29 +0100

    Updates translations and cleans obsolete entries
    Updates the translation files with the latest strings from the source code.
    
    Removes obsolete entries from the translation files to keep them clean and maintainable.
    Also fixes a wrong line number for the "Select app" string in prefs.js.

--- messagingmenu.sh
+++ messagingmenu.sh
@@ -39,6 +39,7 @@ case "$1" in
       do
         echo "Updating: $pofile"
         msgmerge --backup=off -N -U "$pofile" "$reffile"
+        msgattrib --no-obsolete -o "$pofile" "$pofile"
       done
     echo "Done."
     ;;
